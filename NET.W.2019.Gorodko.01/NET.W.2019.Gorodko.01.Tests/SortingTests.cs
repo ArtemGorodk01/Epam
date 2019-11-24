0ccd8c9e5b71a1e42ee43ef53410347730363383
Artem <gorodko_artem@mail.ru>
2019-11-24 11:23:46 +0300

    Fix code style using StyleCop

--- NET.W.2019.Gorodko.01/NET.W.2019.Gorodko.01.Tests/SortingTests.cs
+++ NET.W.2019.Gorodko.01/NET.W.2019.Gorodko.01.Tests/SortingTests.cs
@@ -10,29 +10,18 @@ public class SortingTests
         Assert.ThrowsException<ArgumentNullException>(() => Sorting.QuickSort(null));
     }
 
-    [TestMethod]
-    public void QuickSort_TransformsSourceArraysCorrectly()
+    [DataTestMethod]
+    [DataRow(new int[] { 1 }, new int[] { 1 })]
+    [DataRow(new int[] { 1, 2 }, new int[] { 1, 2 })]
+    [DataRow(new int[] { 2, 3, 1, 5, 4 }, new int[] { 1, 2, 3, 4, 5 })]
+    [DataRow(new int[] { 4, 2, 1, 5, 8, 3, 9, 7, 0, 6 }, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+    [DataRow(new int[] { }, new int[] { })]
+    [DataRow(new int[] { -3, 1, 8, 9, 0, 2, 3, 1 }, new int[] { -3, 0, 1, 1, 2, 3, 8, 9 })]
+    public void QuickSort_TransformsSourceArraysCorrectly(int[] source, int[] expected)
     {
-        var arr1 = new int[] { 1 };
-        var arr2 = new int[] { 1, 2 };
-        var arr3 = new int[] { 2, 3, 1, 5, 4 };
-        var arr4 = new int[] { 4, 2, 1, 5, 8, 3, 9, 7, 0, 6 };
-        var arr5 = new int[] { };
-        var arr6 = new int[] { -3, 1, 8, 9, 0, 2, 3, 1 };
+        Sorting.QuickSort(source);
 
-        Sorting.QuickSort(arr1);
-        Sorting.QuickSort(arr2);
-        Sorting.QuickSort(arr3);
-        Sorting.QuickSort(arr4);
-        Sorting.QuickSort(arr5);
-        Sorting.QuickSort(arr6);
-
-        Assert.IsTrue(AreSame(arr1, new int[] { 1 }));
-        Assert.IsTrue(AreSame(arr2, new int[] { 1, 2 }));
-        Assert.IsTrue(AreSame(arr3, new int[] { 1, 2, 3, 4, 5 }));
-        Assert.IsTrue(AreSame(arr4, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
-        Assert.IsTrue(AreSame(arr5, new int[] { }));
-        Assert.IsTrue(AreSame(arr6, new int[] { -3, 0, 1, 1, 2, 3, 8, 9 }));
+        Assert.IsTrue(AreSame(source, expected));
     }
 
     [TestMethod]
@@ -42,32 +31,31 @@ public class SortingTests
     }
 
     [TestMethod]
-    public void MergeSort_ReturnsRightArray()
+    [DataTestMethod]
+    [DataRow(new int[] { 1 }, new int[] { 1 })]
+    [DataRow(new int[] { 1, 2 }, new int[] { 1, 2 })]
+    [DataRow(new int[] { 2, 3, 1, 5, 4 }, new int[] { 1, 2, 3, 4, 5 })]
+    [DataRow(new int[] { 4, 2, 1, 5, 8, 3, 9, 7, 0, 6 }, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+    [DataRow(new int[] { }, new int[] { })]
+    [DataRow(new int[] { -3, 1, 8, 9, 0, 2, 3, 1 }, new int[] { -3, 0, 1, 1, 2, 3, 8, 9 })]
+    public void MergeSort_TransformsSourceArraysCorrectly(int[] source, int[] expected)
     {
-        var arr1 = new int[] { 1 };
-        var arr2 = new int[] { 1, 2 };
-        var arr3 = new int[] { 2, 3, 1, 5, 4 };
-        var arr4 = new int[] { 4, 2, 1, 5, 8, 3, 9, 7, 0, 6 };
-        var arr5 = new int[] { };
-        var arr6 = new int[] { -3, 1, 8, 9, 0, 2, 3, 1 };
-
-        Assert.IsTrue(AreSame(Sorting.MergeSort(arr1), new int[] { 1 }));
-        Assert.IsTrue(AreSame(Sorting.MergeSort(arr2), new int[] { 1, 2 }));
-        Assert.IsTrue(AreSame(Sorting.MergeSort(arr3), new int[] { 1, 2, 3, 4, 5 }));
-        Assert.IsTrue(AreSame(Sorting.MergeSort(arr4), new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
-        Assert.IsTrue(AreSame(Sorting.MergeSort(arr5), new int[] { }));
-        Assert.IsTrue(AreSame(Sorting.MergeSort(arr6), new int[] { -3, 0, 1, 1, 2, 3, 8, 9 }));
+        Assert.IsTrue(AreSame(Sorting.MergeSort(source), expected));
     }
 
     private bool AreSame(int[] array1, int[] array2)
     {
         if (array1.Length != array2.Length)
+        {
             return false;
+        }
 
         for (int i = 0; i < array1.Length; i++)
         {
             if (array1[i] != array2[i])
+            {
                 return false;
+            }
         }
 
         return true;
--- NET.W.2019.Gorodko.01/NET.W.2019.Gorodko.01/Sorting.cs
+++ NET.W.2019.Gorodko.01/NET.W.2019.Gorodko.01/Sorting.cs
@@ -6,57 +6,70 @@ using System.Collections.Generic;
 /// </summary>
 public static class Sorting
 {
-    ///<summary>
-    ///Orders the array
+    /// <summary>
+    /// Orders the array
     /// </summary>
     /// <param name="array">Array which has to be ordered</param>
     public static void QuickSort(int[] array)
     {
         if (array == null)
+        {
             throw new ArgumentNullException(nameof(array));
+        }
 
         if (array.Length <= 1)
+        {
             return;
+        }
 
-        //contains boards of disordered parts of array
+        // contains boards of disordered parts of array
         var stack = new Stack<(int, int)>();
         stack.Push((0, array.Length - 1));
 
         while (stack.Count != 0)
         {
             (int left, int right) = stack.Pop();
-            Console.WriteLine(left + " " + right);
             int cross = HoarePartition(array, left, right);
 
             if (cross > left)
+            {
                 stack.Push((left, cross));
+            }
+
             if (cross + 1 < right)
+            {
                 stack.Push((cross + 1, right));
+            }
         }
     }
 
-    ///<summary>
-    ///Returns the ordered array
+    /// <summary>
+    /// Returns the ordered array
     /// </summary>
     /// <param name="array">Array which has to be ordered</param>
     /// <returns>Ordered array</returns>
     public static int[] MergeSort(int[] array)
     {
         if (array == null)
+        {
             throw new ArgumentNullException(nameof(array));
+        }
 
         if (array.Length <= 1)
+        {
             return array;
+        }
 
-        //contains sorted arrays
+        // contains sorted arrays
         var queue = new Queue<int[]>();
-        //devide source array 
+
+        // devide source array 
         foreach (var item in array)
         {
             queue.Enqueue(new int[1] { item });
         }
 
-        while(queue.Count != 1)
+        while (queue.Count != 1)
         {
             queue.Enqueue(Merge(queue.Dequeue(), queue.Dequeue()));
         }
@@ -76,11 +89,13 @@ public static class Sorting
             do { } while (array[++i] < pivot && i < high);
             do { } while (array[--j] > pivot && j > low);
 
-            //if indexes are crossed
+            // if indexes are crossed
             if (i >= j)
+            {
                 return j;
+            }
 
-            //else change elements
+            // else change elements
             (array[i], array[j]) = (array[j], array[i]);
         }
     }
@@ -92,12 +107,10 @@ public static class Sorting
         int array1Index = 0, array2Index = 0;
         for (int resIndex = 0; resIndex < resultArray.Length; resIndex++)
         {
-            //if the first array is fully in the result array
             if (array1Index >= array1.Length)
             {
                 resultArray[resIndex] = array2[array2Index++];
             }
-            //if the second array is fully in the result array
             else if (array2Index >= array2.Length)
             {
                 resultArray[resIndex] = array1[array1Index++];
