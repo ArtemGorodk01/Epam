97c834b4a85651908cd9de17fc3bba43560af716
Artem <gorodko_artem@mail.ru>
2019-12-28 19:17:30 +0300

    Add Data.Load module.

--- NET.W.2019.Gorodko.10/Data.Load/Abstract/IDataLoader.cs
+++ NET.W.2019.Gorodko.10/Data.Load/Abstract/IDataLoader.cs
@@ -2,5 +2,6 @@
 {
     public interface IDataLoader
     {
+        string Load();
     }
 }
--- NET.W.2019.Gorodko.10/Data.Load/FileDataLoader.cs
+++ NET.W.2019.Gorodko.10/Data.Load/FileDataLoader.cs
@@ -1,8 +1,27 @@
-﻿using Data.Load.Abstract;
+﻿using System;
+using System.IO;
+using Data.Load.Abstract;
 
 namespace Data.Load
 {
     public class FileDataLoader : IDataLoader
     {
+        private readonly string path;
+
+        public FileDataLoader(string path)
+        {
+            this.path = path ?? throw new ArgumentNullException(nameof(path));
+        }
+
+        public string Load()
+        {
+            string data;
+            using (var reader = new StreamReader(path))
+            {
+                data = reader.ReadToEnd();
+            }
+
+            return data;
+        }
     }
 }
