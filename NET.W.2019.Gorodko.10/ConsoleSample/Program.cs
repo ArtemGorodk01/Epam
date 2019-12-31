8348fe13c079a099a6a7f89304518a7ccd4595a3
Artem <gorodko_artem@mail.ru>
2019-12-31 17:06:16 +0300

    Add console sample.

--- NET.W.2019.Gorodko.10/ConsoleSample/Program.cs
+++ NET.W.2019.Gorodko.10/ConsoleSample/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace ConsoleSample
+﻿using System.IO;
+using Data.Load;
+using Data.Parsing;
+using Data.Parsing.Converting;
+using Data.Parsing.Validation;
+using Xml.Export;
+
+namespace ConsoleSample
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            string path = "test.txt";
+            using (var writer = new StreamWriter(File.Create(path)))
+            {
+                writer.WriteLine("https://github.com/AnzhelikaKravchuk?tab=repositories");
+                writer.WriteLine("https://github.com/AnzhelikaKravchuk/2017-2018.MMF.BSU");
+                writer.WriteLine("https://habrahabr.ru/company/it-grad/blog/341486/");
+            }
+
+            var fileLoader = new FileDataLoader(path);
+            string dataString = fileLoader.Load();
+
+            var urlParser = new UrlParser(new DefaultUrlConverter(), new DefaultUrlValidator());
+            var urls = urlParser.Parse(dataString);
+
+            using (var fs = File.Create("testresult.xml"))
+            {
+                var exporter = new DefaultXmlExporter(fs);
+                exporter.Export(urls);
+            }
         }
     }
 }
