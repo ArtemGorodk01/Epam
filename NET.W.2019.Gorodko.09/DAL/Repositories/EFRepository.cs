0b11d5e79f024eea01d1b1c99321f199996497a7
Artem <gorodko_artem@mail.ru>
2020-01-17 21:39:53 +0300

    Add DAL skeleton

---
+++ NET.W.2019.Gorodko.09/DAL/Repositories/EFRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL.Interface.DTO;
+using DAL.Interface.Interfaces;
+
+namespace DAL.Repositories
+{
+    public class EFRepository : IRepository
+    {
+        public IEnumerable<AccountDTO> Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IEnumerable<AccountDTO> accounts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
